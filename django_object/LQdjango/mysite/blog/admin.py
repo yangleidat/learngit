a3ed8b96eea92e97270470bbda78b997d227eb7c
yangleidat <hdcqyl@126.com>
2018-11-04 00:12:03 +0800

    配置blog的url，并且可以通过点击标题访问正文

--- django_object/LQdjango/mysite/blog/admin.py
+++ django_object/LQdjango/mysite/blog/admin.py
@@ -3,4 +3,13 @@ from .models import BlogArticles
 
 # Register your models here.
 
-admin.site.register(BlogArticles)
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish')
+    list_filter = ('publish', 'author')
+    search_fields = ('title', 'body')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['publish', 'author']
+
+
+admin.site.register(BlogArticles, BlogArticlesAdmin)
--- django_object/LQdjango/mysite/blog/views.py
+++ django_object/LQdjango/mysite/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogArticles
 
 # Create your views here.
+
+def blog_title(request):#request这个参数不能缺少，它负责响应所有接收到的请求，必须在第一个
+    blogs = BlogArticles.objects.all()  # 得到所有BlogArticles中的对象
+    return render(request, 'blog/titles.html', {'blogs':blogs})
+
+def blog_article(request, article_id):
+    #article = BlogArticles.objects.get(id=article_id)
+    article = get_object_or_404(BlogArticles, id=article_id)
+    pub = article.publish
+    return render(request, 'blog/content.html', {'article':article, 'publish':pub})
