e89557b7923ca859750f51d12bdbeecb74b78f33
yangleidat <hdcqyl@126.com>
2018-11-03 22:35:51 +0800

    写了第一篇文章

--- django_object/LQdjango/mysite/blog/admin.py
+++ django_object/LQdjango/mysite/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BlogArticles
 
 # Register your models here.
+
+admin.site.register(BlogArticles)
--- django_object/LQdjango/mysite/blog/models.py
+++ django_object/LQdjango/mysite/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BlogArticles(models.Model):
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(User, related_name="blog_posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publish',)
+        #按照publish字段值的倒序显示
+
+    def __str__(self):
+        return self.title
