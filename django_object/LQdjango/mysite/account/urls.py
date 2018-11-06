c92f8cdcd709082c547944499eecc361eae3e9cb
yangleidat <hdcqyl@126.com>
2018-11-06 23:55:49 +0800

    logout

--- django_object/LQdjango/mysite/account/urls.py
+++ django_object/LQdjango/mysite/account/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 from . import views
 
 from django.conf import settings
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    url(r"^login/$", views.user_login, name="user_login"),
-    
+    #url(r"^login/$", views.user_login, name="user_login"),
+    #url(r'^login/$', auth_views.login, name='user_login'),
+    url(r'^login/$', auth_views.login,
+        {'template_name': 'account/login.html'}, name='user_login'),
+    url(r'^logout/$', auth_views.logout,
+        {'template_name': 'account/logout.html'}, name='user_logout'),
 ]
