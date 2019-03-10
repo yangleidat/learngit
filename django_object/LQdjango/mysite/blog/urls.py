54f0f8bfcf7598a02feaacf43ba92149006f5440
yangleidat <hdcqyl@126.com>
2019-03-10 11:57:23 +0800

    sudoku

--- django_object/LQdjango/mysite/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.blog_title, name='blog_title'),#name在HTML模板中配合项目url下的指向这里的namespace使用
-    url(r'(?P<article_id>\d)/$', views.blog_article, name='blog_detail'),
-]
--- django_object/khpj/shoujishuju/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShoujishujuConfig(AppConfig):
-    name = 'shoujishuju'
